937d58dd08857215ff45ba8e8d46de931296c2f2
wzyacc <wzyacc@gmail.com>
2020-01-10 13:53:13 +0800

    pass

--- gllearn/game.h
+++ gllearn/game.h
@@ -8,10 +8,13 @@
 ******************************************************************/
 #ifndef GAME_H
 #define GAME_H
+#include <vector>
+#include <tuple>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "game_level.h"
 
 // Represents the current state of the game
 enum GameState {
@@ -19,6 +22,25 @@ enum GameState {
     GAME_MENU,
     GAME_WIN
 };
+
+// Represents the four possible (collision) directions
+enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+// Defines a Collision typedef that represents collision data
+typedef std::tuple<GLboolean, Direction, glm::vec2> Collision; // <collision?, what direction?, difference vector center - closest point>
+
+// Initial size of the player paddle
+const glm::vec2 PLAYER_SIZE(100, 20);
+// Initial velocity of the player paddle
+const GLfloat PLAYER_VELOCITY(500.0f);
+// Initial velocity of the Ball
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+// Radius of the ball object
+const GLfloat BALL_RADIUS = 12.5f;
 
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
@@ -30,6 +52,8 @@ public:
     GameState              State;
     GLboolean              Keys[1024];
     GLuint                 Width, Height;
+    std::vector<GameLevel> Levels;
+    GLuint                 Level;
     // Constructor/Destructor
     Game(GLuint width, GLuint height);
     ~Game();
@@ -39,6 +63,10 @@ public:
     void ProcessInput(GLfloat dt);
     void Update(GLfloat dt);
     void Render();
+    void DoCollisions();
+    // Reset
+    void ResetLevel();
+    void ResetPlayer();
 };
 
 #endif
